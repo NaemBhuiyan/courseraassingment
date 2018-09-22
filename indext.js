92aafb4635f20289c665673d76e073c769a4cf4c
Md.Naem Bhuyian <hopingboynaem@gmail.com>
2018-09-22 07:08:57 +0600

    2nd comitt

---
+++ indext.js
@@ -0,0 +1,12 @@
+
+// function myFucntion(params) 
+// {
+//     var greetingString ="hello";
+//     var myName=prompt("please enter your name");
+//     var concatString;
+//     // document.write(greetingString + " "+myName+"<br>");
+//     concatString=greetingString+" "+myName;
+//     // document.write(concatString);
+//     document.getElementById("demo").innerHTML="hello" +" "+ myName;   
+// }
+// myFucntion();
